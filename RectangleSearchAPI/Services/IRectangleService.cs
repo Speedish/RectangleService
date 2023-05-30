fd7db3b75cd60066d7732ab1dd25eca8427c8490
Jayawardana Welathanthrige Ishan Botejue <200518446@student.georgianc.on.ca>
2023-05-30 00:29:40 +0000

    Deleted RectangleSearchAPI

--- RectangleSearchAPI/Services/IRectangleService.cs
+++
@@ -1,9 +0,0 @@
-﻿using RectangleSearchAPI.DataAccess.Domain.Models;
-
-namespace RectangleSearchAPI.Web.Services
-{
-    public interface IRectangleService
-    {
-        public List<Rectangle> SearchRectangles(CoordinateInput input);
-    }
-}
